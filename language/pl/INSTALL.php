f3937e72d05d92766185bda974eb1647cf8103b1
Gary <fishermanpl@gmail.com>
2012-12-17 12:26:46 +0100

    Poprawiona stylistyka INSTALL.php [PL].

--- language/pl/INSTALL.php
+++ language/pl/INSTALL.php
@@ -1,30 +1,11 @@
 <?php
 
 /**
- *  2Moons
- *  Copyright (C) 2011 Jan Kröpke
+ *  Tarion.net
+ *  All rights reversed (C) 2012
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *  Translated into Polish by Alojz and Gary (Tarion.net).
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package 2Moons
- * @author Jan Kröpke <info@2moons.cc>
- * @copyright 2009 Lucky
- * @copyright 2011 Jan Kröpke <info@2moons.cc>
- * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
- * @version 1.5 (2011-07-31)
- * @info $Id: INSTALL.php 2263 2012-06-27 22:55:52Z slaver7 $
- * @link http://code.google.com/p/2moons/
  */
 
 $LNG['back']					= 'Wróć';
@@ -118,5 +99,5 @@ $LNG['step6_info_additional']	= 'Po kliknięciu zostaniesz przeniesiony na stron
 
 $LNG['sql_close_reason']		= 'Gra chwilowo zamknięta';
 $LNG['sql_welcome']				= 'Serdecznie witamy na planecie v';
-// Translated into Polish by Sirgomo&Alojz. All rights reversed (C) 2012
+
 ?>
